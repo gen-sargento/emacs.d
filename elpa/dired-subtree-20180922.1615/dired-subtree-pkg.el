8c64a1e749d7d98d0364c7785aa0e96f67b5c065
Genesis <genesistobiassargento@gmail.com>
2019-10-18 17:37:07 +0800

    Initial commit

---
+++ elpa/dired-subtree-20180922.1615/dired-subtree-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-subtree" "20180922.1615" "Insert subdirectories in a tree-like fashion" '((dash "2.5.0") (dired-hacks-utils "0.0.1")) :commit "ae39981d7cc58206cfeb377d6621bdd9000fd472" :keywords '("files") :authors '(("Matúš Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matúš Goljer" . "matus.goljer@gmail.com"))
